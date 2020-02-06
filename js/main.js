b8db0c04facd93ff6b7905c52f46688d8b52a088
LightDev1 <karumasanmy@gmail.com>
2020-02-06 19:38:08 +0400

    Homework3

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
 let mySwiper = new Swiper('.swiper-container', {
-    slidesPerView: 2,
+    slidesPerView: 1,
     loop: true,
     navigation: {
         nextEl: '.arrow',
     },
+    breakpoints: {
+        540: {
+          slidesPerView: 2,
+        }
+      }
+});
+
+const menuButton = document.querySelector('.menu-button'),
+    menu = document.querySelector('.header');
+
+menuButton.addEventListener('click', () => {
+    menuButton.classList.toggle('menu-button-active');
+    menu.classList.toggle('header-active');
 });
